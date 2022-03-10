cdd2f609b74d98e6d6ef38509964153da7c548d4
Faza Ghulam Ahmad <fazaghulam@gmail.com>
2022-03-10 12:08:17 +0700

    add font style

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link href="http://fonts.cdnfonts.com/css/gilroy-bold" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
